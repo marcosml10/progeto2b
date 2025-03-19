2bec7df4fe73e4a3bd640f082ca384a56557691d
marcoslima60 <marcosdemoraeslima60@gmail.com>
2025-03-19 19:52:28 -0300

    criando um progeto de login

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace progeto2b.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
